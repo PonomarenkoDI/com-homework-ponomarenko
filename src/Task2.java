580e9a9cf74e64c0172608098a3b8dc0cfe361f5
Alex Bobrov <b-aleks05076969@mail.ru>
2022-07-11 20:59:48 +0300

    added new files

---
+++ src/Task2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner privet = new Scanner(System.in);
+        System.out.println("Введите время: ");
+        int time = privet.nextInt();
+        if (time >=0 && time <6){
+        System.out.println("Доброй ночи");
+       } else if (time > 6 && time <12 ){
+        System.out.println("Добрый утро");
+
+
+
+
+
+    }
+}}
