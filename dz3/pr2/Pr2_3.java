b02775f23a9d48f561f92cbb0b21012795bdefa9
Alex Bobrov <b-aleks05076969@mail.ru>
2022-07-21 18:34:02 +0300

    added new files

---
+++ dz3/pr2/Pr2_3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Pr2_3 {
+    public static void main(String[] args) {
+       final int time = 60;
+       final int distation = 100;
+       int speed = distation / time;
+        System.out.println(speed +" km/t");
+
+    }
+}
